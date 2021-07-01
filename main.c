459ded9d584f33a4f45282731c3ef01f217e8b97
Srivatsa6 <84716329+Srivatsa6@users.noreply.github.com>
2021-07-01 09:59:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,58 @@
+
 #include <stdio.h>
-#include <assert.h>
 #include <string.h>
+#include <ctype.h>
+#include <math.h>
 #include <cs50.h>
 
-string ari(string);
-
-
-void test1() {
-  string s1="The town was large enough to support three inns, and all their common rooms were rife with rumors. Arianne sent her men into each of them, to hear what they might hear. In the Broken Shield, Daemon Sand was told that the great septry on the Holf of Men had been burned and looted by raiders from the sea, and a hundred young novices from the motherhouse on Maiden Isle carried off into slavery.";
-  assert(strcmp(ari(s1), "Eleventh Grade") == 0);
+string ari(string s){
+    int chars = 0, words = 0, sens = 0, a;
+    float f_1, f_2, t_1, t_2;
+    for (int i = 0; i < strlen(s); i++){
+        if (isalnum(s[i])){
+            chars++;
+        }
+        if (s[i] == ' '){
+            words++;
+        }
+        if (s[i] == '.' || s[i] == '!' || s[i] == '?'){
+            sens++;
+        }
+    }
+    f_1 = (double)chars/words;
+    f_2 = (double)words/sens;
+    t_1 = 4.71 * f_1;
+    t_2 = (0.5 * f_2) - 21.43;
+    a = ceil(t_1 + t_2);
+    switch (a){
+    case 1:
+        return "Kindergarten";
+    case 2:
+        return "First/Second Grade";
+    case 3:
+        return "Third Grade";
+    case 4:
+        return "Fourth Grade";
+    case 5:
+        return "Fifth Grade";
+    case 6:
+        return "Sixth Grade";
+    case 7:
+        return "Seventh Grade";
+    case 8:
+        return "Eight Grade";
+    case 9:
+        return "Ninth Grade";
+    case 10:
+        return "Tenth Grade";
+    case 11:
+        return "Eleventh Grade";
+    case 12:
+        return "Twelfth Grade";
+    case 13:
+        return "College student";
+    case 14:
+        return "Professor";
+    }
 }
-
-void test2() { 
- string s2="A mouse took a stroll through the deep dark wood. A fox saw the mouse, and the mouse looked good. Where are you going to, little brown mouse? Come and have lunch in my underground house.";
-  assert(strcmp(ari(s2), "Third Grade") == 0);
-}
-
-void test3() {
-  string s3="Exhaled respiratory particles can build-up within enclosed spaces with inadequate ventilation. The risk of COVID-19 infection increases especially in spaces where people engage in physical exertion or raise their voice (e.g., exercising, shouting, singing) as this increases exhalation of respiratory droplets. Prolonged exposure to these conditions, typically more than 15 minutes, leads to higher risk of infection.";
-  assert(strcmp(ari(s3), "Professor") == 0);
- }
-
-int main(void) {
-  test1();
-  test2();
-  test3();
-}
-
 
